850e8b5d71c66f4f7e4d68c0ac2a60546e987493
hamed <hamedpooramiri1007@gmail.com>
2023-12-01 18:53:42 +0330

    Initial Commit

---
+++ ReduxSwift/ReduxSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxSwiftApp.swift
+//  ReduxSwift
+//
+//  Created by hamedpouramiri on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ReduxSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
